463abad11cf7ac80e9224fc226a9663a4d7aa8dc
Ashley H <ashleyho887@gmail.com>
2023-05-04 18:24:24 -0700

    font sizing styled

--- client/app/theme.tsx
+++ client/app/theme.tsx
@@ -2,12 +2,15 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
 let crAppTheme = createTheme({
   palette: {
-    mode: "light",
     primary: {
-      main: "#D4A373",
+      main: "#FEFAE0",
+      light: "#FAEDCD",
+      dark: "#D4A373",
     },
     secondary: {
-      main: "#FAEDCD",
+      main: "#EDDCD2",
+      light: "#CB997E",
+      dark: "#A5A58D",
     },
     error: {
       main: "#EA0000",
@@ -16,55 +19,64 @@ let crAppTheme = createTheme({
       main: "#f89446",
     },
     success: {
-      main: "#46f855",
+      main: "#0bb000",
     },
     text: {
       primary: "#000",
+      secondary: "#D4A373",
       disabled: "#838282",
     },
     background: {
-      default: "#fefae0",
-    },
-    ...{
-      mode: "dark",
-      palette: {
-        primary: {
-          main: "#A5A58D",
-        },
-        secondary: {
-          main: "#CB997E",
-        },
-        error: {
-          main: "#EA0000",
-        },
-        warning: {
-          main: "#f89446",
-        },
-        success: {
-          main: "#46f855",
-        },
-        text: {
-          primary: "#000",
-          disabled: "#323232",
-        },
-        background: {
-          default: "#EDDCD2",
-        },
-      },
+      default: "#FFF",
     },
   },
   typography: {
     fontFamily: "Helvetica",
+    h1: {
+      fontSize: "42pt",
+      fontWeight: 700,
+      lineHeight: 1.2,
+      marginBottom: "1.5rem",
+    },
+    h2: {
+      fontSize: "36pt",
+      fontWeight: 700,
+      lineHeight: 1.2,
+      marginBottom: "1.25rem",
+    },
+    h3: {
+      fontSize: "32pt",
+      fontWeight: 700,
+      lineHeight: 1.2,
+      marginBottom: "1rem",
+    },
+    h4: {
+      fontSize: "28pt",
+      fontWeight: 700,
+      lineHeight: 1.2,
+      marginBottom: "1rem",
+    },
+    h5: {
+      fontSize: "24pt",
+      fontWeight: 700,
+      lineHeight: 1.2,
+      marginBottom: "0.75rem",
+    },
+    body1: {
+      fontSize: "20pt",
+      fontWeight: 400,
+      lineHeight: 1.5,
+      marginBottom: "1.25rem",
+    },
+    body2: {
+      fontSize: "16pt",
+      fontWeight: 400,
+      lineHeight: 1.5,
+      marginBottom: "1rem",
+    },
   },
   shape: {
     borderRadius: 10,
-  },
-  overrides: {
-    MuiLink: {
-      root: {
-        color: "#000",
-      },
-    },
   },
 });
 
--- client/features/styles/StyleGuide.tsx
+++ client/features/styles/StyleGuide.tsx
@@ -1,38 +1,218 @@
 import React from "react";
-import crAppTheme from "../../app/theme";
+import {
+  ThemeProvider,
+  CssBaseline,
+  Typography,
+  Container,
+  Box,
+} from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { ThemeProvider, Typography, Container, Box } from "@mui/material";
+import crAppTheme from "../../app/theme";
 
-const MainContainer = styled(Container)(({ theme }) => ({
+const MainContainer = styled(Container)({
   width: "100%",
-  height: "100%",
-  position: "absolute",
   top: 0,
   left: 0,
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "flex-start",
-  alignItems: "flex-start",
-  backgroundColor: theme.palette.background.default,
-  color: theme.palette.text.primary,
   margin: 0,
-  padding: 0,
-}));
+  padding: 2,
+  backgroundColor: `${crAppTheme.palette.primary.main}`,
+  color: `${crAppTheme.palette.text.primary}`,
+});
+
+const PaletteBox = styled(Box)({
+  display: "flex",
+  margin: 2,
+  alignItems: "center",
+  justifyContent: "center",
+  border: `1px solid ${crAppTheme.palette.primary.light}`,
+  width: "calc((100vw - 10px) / 5)",
+  minHeight: "calc((100vh - 200px) / 5)",
+  textAlign: "center",
+  textTransform: "uppercase",
+});
 
 const StyleGuide = () => {
   return (
     <ThemeProvider theme={crAppTheme}>
-      <MainContainer>
-        <Typography variant="h1">crAPP the Map's Style Guide</Typography>
-        {/* Color Palette */}
-        <Container>
-          <Box sx={{ bgcolor: "primary.main" }}></Box>
+      <CssBaseline />
+      <MainContainer
+        sx={{
+          alignItems: "center",
+          alignContent: "center",
+        }}
+      >
+        <Typography variant="h1" sx={{ textAlign: "center" }}>
+          crAPP's Style Guide
+        </Typography>
+
+        <Typography variant="h5" sx={{ textAlign: "center", margin: 2 }}>
+          Color Palette
+        </Typography>
+        <Typography sx={{ textAlign: "center" }}>Light Theme</Typography>
+        <Container
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <PaletteBox sx={{ backgroundColor: crAppTheme.palette.primary.main }}>
+            <Typography variant="body1" sx={{ textAlign: "center" }}>
+              {crAppTheme.palette.primary.main}
+            </Typography>
+          </PaletteBox>
+          <PaletteBox
+            sx={{
+              backgroundColor: `${crAppTheme.palette.primary.light}`,
+            }}
+          >
+            <Typography variant="body1">
+              {crAppTheme.palette.primary.light}
+            </Typography>
+          </PaletteBox>
+          <PaletteBox
+            sx={{
+              backgroundColor: `${crAppTheme.palette.primary.dark}`,
+            }}
+          >
+            <Typography variant="body1">
+              {crAppTheme.palette.primary.dark}
+            </Typography>
+          </PaletteBox>
         </Container>
+        <Typography sx={{ textAlign: "center" }}>Dark Theme</Typography>
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <PaletteBox
+            sx={{ backgroundColor: crAppTheme.palette.secondary.main }}
+          >
+            <Typography variant="body1" sx={{ textAlign: "center" }}>
+              {crAppTheme.palette.secondary.main}
+            </Typography>
+          </PaletteBox>
+          <PaletteBox
+            sx={{
+              backgroundColor: `${crAppTheme.palette.secondary.light}`,
+            }}
+          >
+            <Typography variant="body1">
+              {crAppTheme.palette.secondary.light}
+            </Typography>
+          </PaletteBox>
+          <PaletteBox
+            sx={{
+              backgroundColor: `${crAppTheme.palette.secondary.dark}`,
+            }}
+          >
+            <Typography variant="body1">
+              {crAppTheme.palette.secondary.dark}
+            </Typography>
+          </PaletteBox>
+        </Container>
+        <Typography sx={{ textAlign: "center" }}>Status Colors</Typography>
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <PaletteBox
+            sx={{
+              backgroundColor: crAppTheme.palette.error.main,
+            }}
+          >
+            <Typography variant="body1">
+              Error: {crAppTheme.palette.error.main}
+            </Typography>
+          </PaletteBox>
+          <PaletteBox
+            sx={{
+              backgroundColor: crAppTheme.palette.warning.main,
+            }}
+          >
+            <Typography variant="body1">
+              Warning: {crAppTheme.palette.warning.main}
+            </Typography>
+          </PaletteBox>
+          <PaletteBox
+            sx={{
+              backgroundColor: crAppTheme.palette.success.main,
+            }}
+          >
+            <Typography variant="body1">
+              Success: {crAppTheme.palette.success.main}
+            </Typography>
+          </PaletteBox>
+        </Container>
+
+        <Typography variant="h5" sx={{ textAlign: "center", margin: 2 }}>
+          Fonts
+        </Typography>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="h1">Heading 1: viverra</Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="h2">Heading 2: ligula ullamcorper</Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="h3">Heading 3: amet consectetur</Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="h4">Heading 4: vulputate sapien</Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="h5">
+            Heading 5: massa eget egestas purus viverra accumsan in nisl
+          </Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="body1">
+            Body 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+            interdum velit laoreet id donec ultrices.
+          </Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="body2">
+            Body 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+            interdum velit laoreet id donec ultrices.
+          </Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="subtitle1">
+            Subtitle 1: non nisi est sit amet facilisis magna etiam
+          </Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="subtitle2">
+            Subtitle 2: non nisi est sit amet facilisis magna etiam
+          </Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="caption">
+            Caption: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+        </Box>
+        <Box sx={{ textTransform: "capitalize", textAlign: "center" }}>
+          <Typography variant="overline">
+            Overline: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+        </Box>
       </MainContainer>
     </ThemeProvider>
   );
 };
 
-export { MainContainer };
-
 export default StyleGuide;
+
+export { MainContainer };
