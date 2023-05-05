c025938597c5155047e0d7f441633bbdb9f8ee94
fionaxu2023 <xuliefan@hotmail.com>
2023-05-05 03:20:26 -0400

    geolocatefinish

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -7,41 +7,50 @@ import NavBar from "../features/navigation/Navbar";
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useRef, useState, useEffect } from "react";
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hlZXNvbyIsImEiOiJjbGhhcDdjamMwamk5M2hvZ3NmeGlxeW16In0.yFqw0jGTNTtzqqcESkJlWA'
 
 
 const App = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(-73.98);
+const [lat, setLat] = useState(40.76);
+const [zoom, setZoom] = useState(12);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
-    container: mapContainer.current,
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [lng, lat],
-    zoom: zoom
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
     });
-  });
 
-  // useEffect(() => {
-  //   if (!map.current) return; // wait for map to initialize
-  //   map.current.on('move', () => {
-  //   setLng(map.current.getCenter().lng.toFixed(4));
-  //   setLat(map.current.getCenter().lat.toFixed(4));
-  //   setZoom(map.current.getZoom().toFixed(2));
-  //   });
-  // });
+    map.current.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+        position:'top-left',
+        showUserLocation:true,
+      })
+    );
+
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  },[]);
+
   return (
     <ThemeProvider theme={crAppTheme}>
       <CssBaseline />
       <NavBar />
       <AppRoutes />
-      <div ref={mapContainer} className="map-container" />
+      <div ref={mapContainer} className="map-container"></div>
     </ThemeProvider>
   );
 };
