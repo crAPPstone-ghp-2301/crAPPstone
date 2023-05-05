21cb82435231558784173c26b7ccffa828261f62
Hesoo Cha <hesoocha@Camdens-MacBook-Pro.local>
2023-05-05 12:17:09 -0400

    map view in app

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -2,12 +2,42 @@ import React from "react";
 
 import Navbar from "../features/navbar/Navbar";
 import AppRoutes from "./AppRoutes";
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { useRef, useState, useEffect } from "react";
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hlZXNvbyIsImEiOiJjbGhhcDdjamMwamk5M2hvZ3NmeGlxeW16In0.yFqw0jGTNTtzqqcESkJlWA'
 
 const App = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [lng, lat],
+    zoom: zoom
+    });
+  });
+
+  // useEffect(() => {
+  //   if (!map.current) return; // wait for map to initialize
+  //   map.current.on('move', () => {
+  //   setLng(map.current.getCenter().lng.toFixed(4));
+  //   setLat(map.current.getCenter().lat.toFixed(4));
+  //   setZoom(map.current.getZoom().toFixed(2));
+  //   });
+  // });
   return (
     <div>
       <Navbar />
       <AppRoutes />
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 };
--- client/index.tsx
+++ client/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./app/store";
 import App from "./app/App";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '../public/style.css'
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = createRoot(document.getElementById("app"));
