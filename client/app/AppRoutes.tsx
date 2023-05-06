31a00ba68ea1df4f4a674bedc5cf0cfd2936844f
Ashley H <ashleyho887@gmail.com>
2023-05-06 12:10:30 -0700

    scrapped version 6 and restarting

--- client/app/AppRoutes.tsx
+++ client/app/AppRoutes.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import crAppTheme from "./theme";
 import { ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
-import Home from "../features/home/Home";
 import StyleGuide from "../features/styles/StyleGuide";
-import Profile from "../features/navigation/Profile";
 
 /**
  * COMPONENT
@@ -14,10 +12,7 @@ const AppRoutes = () => {
   return (
     <ThemeProvider theme={crAppTheme}>
       <Routes>
-        {/* <Route path="/*" element={<Home />} />
-        <Route to="/home" element={<Home />} /> */}
         <Route path="/styleguide" element={<StyleGuide />} />
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </ThemeProvider>
   );
---
+++ client/features/navigation/SideBar.tsx
@@ -0,0 +1,113 @@
+import * as React from "react";
+import crAppTheme from "../../app/theme";
+import {
+  ThemeProvider,
+  Container,
+  Drawer,
+  List,
+  ListItem,
+  IconButton,
+  useMediaQuery,
+  CssBaseline,
+  Divider,
+  Typography,
+} from "@mui/material";
+import SignIn from "./SignIn";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import BookmarkBorderRoundedIcon from "@mui/icons-material/BookmarkBorderRounded";
+import HistoryRoundedIcon from "@mui/icons-material/HistoryRounded";
+
+const SideBar = () => {
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const isMobile = useMediaQuery("(max-width:700px)");
+
+  const toggleDialog = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <ThemeProvider theme={crAppTheme}>
+      <CssBaseline />
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        open={isMobile ? drawerOpen : true}
+        onClose={handleDrawerToggle}
+      >
+        <List
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <ListItem sx={{ justifyContent: "center", my: 1 }}>
+            <MenuRoundedIcon fontSize="large" />
+          </ListItem>
+          <ListItem
+            sx={{
+              justifyContent: "center",
+              my: 1,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <BookmarkBorderRoundedIcon fontSize="large" />
+            <Typography variant="overline" sx={{ textTransform: "capitalize" }}>
+              Saved
+            </Typography>
+          </ListItem>
+          <ListItem
+            sx={{
+              justifyContent: "center",
+              my: 1,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <HistoryRoundedIcon fontSize="large" />
+            <Typography variant="overline" sx={{ textTransform: "capitalize" }}>
+              History
+            </Typography>
+          </ListItem>
+          <Divider />
+          <ListItem sx={{ justifyContent: "center", my: 1 }}>
+            <SignIn />
+          </ListItem>
+        </List>
+      </Drawer>
+      {isMobile && (
+        <Container
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            zIndex: 1,
+          }}
+        >
+          <IconButton
+            onClick={handleDrawerToggle}
+            sx={{
+              borderRadius: 50,
+              p: 1,
+              backgroundColor: crAppTheme.palette.primary.main,
+              color: crAppTheme.palette.text.secondary,
+            }}
+          >
+            <MenuRoundedIcon />
+          </IconButton>
+          <SignIn />
+        </Container>
+      )}
+    </ThemeProvider>
+  );
+};
+
+export default SideBar;
