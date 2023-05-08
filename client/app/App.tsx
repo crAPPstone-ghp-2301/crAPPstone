86503c95056810244a537792d08b3c815d9c36a4
ssane005 <ssane005@fiu.edu>
2023-05-08 12:14:24 -0400

    changes from weekend

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline, Container } from "@mui/material";
 import crAppTheme from "./theme";
 import AppRoutes from "./AppRoutes";
-import NavBar from "../features/navigation/Navbar";
+import NavBar from "../features/navigation/NavBar";
 import Map from "./Map";
 
 
---
+++ client/features/restrooms/allRestroomSlice.tsx
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAllRestrooms = createAsyncThunk("restrooms/getAll", async () => {
+  try {
+    console.log("ALL restrooms AXIOS WORKING");
+    const { data } = await axios.get(`/api/restrooms`);
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+});
+
+export const allRestroomsSlice = createSlice({
+  name: "restrooms",
+  initialState: {
+    allRestrooms: [],
+    error: null,
+    status: "idle",
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllRestrooms.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(getAllRestrooms.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectRestroom = (state) => {
+  return state.restrooms;
+};
+
+export default allRestroomsSlice.reducer;
