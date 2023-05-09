9ec140b967af4dc6dccfa3a574abe1958fb4e341
fionaxu2023 <xuliefan@hotmail.com>
2023-05-09 03:25:20 -0400

    change

--- client/app/Map.tsx
+++ client/app/Map.tsx
@@ -5,9 +5,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { useRef, useState, useEffect } from "react";
 import { enableMapSet } from 'immer';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
-import {Button} from "@mui/material";
-import {CustomizedIconButton} from "../features/styles/StyleGuide"
-import DirectionsIcon from "@mui/icons-material/Directions";
+import {Button, Typography} from "@mui/material";
+import { PrimaryButton} from "../features/styles/StyleGuide"
+
 
 const restrooms = [
   {
@@ -150,7 +150,8 @@ const Map = () => {
         const popup = new mapboxgl.Popup({ offset: [0, -15] })
         .setLngLat(feature.geometry.coordinates)
         .setHTML(
-        `<h3>${feature.properties.name}</h3><p>${feature.properties.description}</p>`
+        `<h3>${feature.properties.name}</h3>
+        <p>${feature.properties.description}</p>`
         )
         .addTo(map.current);
         });
@@ -161,13 +162,10 @@ const Map = () => {
 
       const geocoder = new MapboxGeocoder({
         accessToken: mapboxgl.accessToken,
-        marker: true,
+        mapboxgl: mapboxgl,
+        marker:true, 
     });
       
-      geocoder.on('result', (event) => {
-        const searchText = event.result.text;
-        console.log(searchText);
-      });
 
       map.current.addControl(geocoder,"top-right");
       
@@ -176,34 +174,10 @@ const Map = () => {
         setLat(map.current.getCenter().lat.toFixed(4));
         setZoom(map.current.getZoom().toFixed(2));
     });
-
     map.current.on('load', function() {
-
-      // Search Places Result Event
-      geocoder.on('result', function(event) {
-          console.log(event.result);
-          new Promise(function(resolve) {
-              // Removing Previous Search Result Marker
-              $('.marker').remove()
-              resolve()
-          }).then(() => {
-              // Adding Marker to the place
-              new mapboxgl.Marker($('<div class="marker"><i class="fa fa-map-marker-alt"></i></div>')[0])
-                  .setLngLat(event.result.geometry.coordinates)
-                  .setPopup(
-                      new mapboxgl.Popup({ offset: 25 }) // add popups
-                      .setHTML(
-                          `<div>${event.result.place_name}</div><small class='text-muted'>${parseFloat(event.result.center[0]).toLocaleString('en-US')}, ${parseFloat(event.result.center[1]).toLocaleString('en-US')}</small>`
-                      )
-                  )
-                  .addTo(map.current)
-          }).then(() => {
-              $('.marker').click()
-          })
-  
-      });
       geocoder.container.setAttribute('id', 'geocoder-search')
   });
+  
 
   function direction_reset() {
     directions.actions.clearOrigin()
@@ -238,17 +212,21 @@ $(function() {
   return (
     <div>
     <div ref={mapContainer} className="map-container"></div>
-    <div className="position-absolute top-0 start-50 translate-middle-x">
-  <div className="d-flex justify-content-center">
-    <button className="btn btn-lg btn-light bg-gradient bg-light border mt-3 mx-auto" id="get-direction">
-      <i className="fa fa-directions"></i> Direction
-    </button>
-    <button className="btn btn-lg btn-light bg-gradient bg-light border mx-auto d-none" id="end-direction">
-      <i className="fa fa-times"></i> End Direction
-    </button>
+    <div className="position-absolute top-0 start-6 ">
+    <PrimaryButton variant="outlined" sx={{ mr: 2, mt: 3 }} id="get-direction">
+      <Typography variant="subtitle1" sx={{ textTransform: "capitalize" }}>
+        Direction
+      </Typography>
+      <i className="fa fa-directions"></i>
+    </PrimaryButton>
+    <PrimaryButton variant="outlined" sx={{ mt: 3 }} className="d-none" id="end-direction">
+      <Typography variant="subtitle1" sx={{ textTransform: "capitalize" }}>
+        End Direction
+      </Typography>
+      <i className="fa fa-times"></i>
+    </PrimaryButton>
   </div>
 </div>
-  </div>
   )
 }
 
