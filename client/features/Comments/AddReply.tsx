10dabcf10c30bb8bf78a5249d4f29dd9afb9cc0b
jnnsn <jennie.son2@gmail.com>
2023-05-09 00:36:17 -0400

    in progress of fixing AddReply

--- client/features/Comments/AddReply.tsx
+++ client/features/Comments/AddReply.tsx
@@ -12,98 +12,78 @@ import {
   Typography,
 } from "@mui/material";
 
-// const AddReply = ({ reviewId, commentId }) => {
-//   const dispatch = useDispatch();
-//   const [content, setContent] = useState("");
-//   // const { reviewId, commentId } = useParams();
-
-//   const handleChange = (event) => {
-//     setContent(event.target.value);
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     dispatch(createReply({ content }));
-//     setContent("");
-//   };
-
-//   return (
-//     <ThemeProvider theme={crAppTheme}>
-//       <Box
-//         component="form"
-//         sx={{
-//           display: "flex",
-//           flexDirection: "column",
-//           alignItems: "flex-start",
-//           width: "50%",
-//           margin: "0 auto",
-//         }}
-//       >
-//         <Container>
-//           <TextField
-//             label="Add Reply"
-//             value={content}
-//             onChange={handleChange}
-//             fullWidth
-//             variant="outlined"
-//             margin="normal"
-//           />
-
-//           <SecondaryButton
-//             type="submit"
-//             variant="contained"
-//             // color="primary"
-//             disabled={!content.trim()}
-//             onClick={handleSubmit}
-//             style={{ marginTop: "10px" }}
-//           >
-//             <Typography variant="subtitle1" sx={{ textTranform: "capitalize" }}>
-//               Submit
-//             </Typography>
-//           </SecondaryButton>
-
-//           <SecondaryButton
-//             type="button"
-//             variant="contained"
-//             disabled={!content.trim()}
-//             style={{ marginTop: "10px" }}
-//           >
-//             <Typography variant="subtitle1" sx={{ textTranform: "capitalize" }}>
-//               Cancel
-//             </Typography>
-//           </SecondaryButton>
-//         </Container>
-//       </Box>
-//     </ThemeProvider>
-//   );
-// };
-
-// export default AddReply;
-
-
 const AddReply = ({ reviewId, commentId }) => {
   const dispatch = useDispatch();
+
   const [content, setContent] = useState("");
 
-  const onSubmit = (event) => {
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
     dispatch(createReply({ reviewId, commentId, content }));
     setContent("");
   };
 
+  const handleReset = (event) => { 
+    event.preventDefault();
+    setContent("");
+  }
+
   return (
-    <form onSubmit={onSubmit}>
-      <label>
-        <input
-          type="text"
-          value={content}
-          onChange={(event) => setContent(event.target.value)}
-        />
-      </label>
-    
-      <button type="submit">Submit</button>
-    </form>
+    <ThemeProvider theme={crAppTheme}>
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          width: "50%",
+          margin: "0 auto",
+        }}
+      >
+        <Container>
+          <TextField
+            label="Add Reply"
+            value={content}
+            onChange={handleChange}
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+
+          <SecondaryButton
+            type="submit"
+            variant="contained"
+            // color="primary"
+            disabled={!content.trim()}
+            onClick={handleSubmit}
+            style={{ marginTop: "10px" }}
+          >
+            <Typography variant="subtitle1" sx={{ textTranform: "capitalize" }}>
+              Submit
+            </Typography>
+          </SecondaryButton>
+
+          <SecondaryButton
+            type="button"
+            variant="contained"
+            disabled={!content.trim()}
+            onClick={handleReset}
+            style={{ marginTop: "10px" }}
+
+          >
+            <Typography variant="subtitle1" sx={{ textTranform: "capitalize" }}>
+              Cancel
+            </Typography>
+          </SecondaryButton>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 };
 
 export default AddReply;
+
