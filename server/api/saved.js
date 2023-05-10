eceead9d3c844574babacf9d1aa4097af3cd585b
Hesoo Cha <hesoocha@Camdens-MacBook-Pro.local>
2023-05-10 17:49:21 -0400

    added routes for saved

--- server/api/saved.js
+++ server/api/saved.js
@@ -1,4 +1,14 @@
 const router = require('express').Router()
 //route for saved restrooms
+const { models: { Favorites }} = require('../db')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const saved = await Favorites.findAll()
+    res.json(saved)
+  } catch (err) {
+    next(err)
+  }
+})
 
 module.exports = router
