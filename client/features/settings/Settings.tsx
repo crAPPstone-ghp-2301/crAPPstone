faa3a6fd250cb468788d236b044e20f57685b8ae
Ashley H <ashleyho887@gmail.com>
2023-05-10 14:19:51 -0700

    updated settings

--- client/features/settings/Settings.tsx
+++ client/features/settings/Settings.tsx
@@ -9,12 +9,17 @@ import {
   ListItemAvatar,
   Avatar,
   Typography,
+  Divider,
+  Container,
 } from "@mui/material";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 import AddLocationAltRoundedIcon from "@mui/icons-material/AddLocationAltRounded";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import BookmarkBorderRoundedIcon from "@mui/icons-material/BookmarkBorderRounded";
 import HistoryRoundedIcon from "@mui/icons-material/HistoryRounded";
+import ReviewsRoundedIcon from "@mui/icons-material/ReviewsRounded";
+import LinkRoundedIcon from "@mui/icons-material/LinkRounded";
+import InfoRoundedIcon from "@mui/icons-material/InfoRounded";
 
 const Settings = ({ open, onClose }) => {
   return (
@@ -36,41 +41,76 @@ const Settings = ({ open, onClose }) => {
           >
             <CloseRoundedIcon />
           </TertiaryButton>
-          <ListItem>
-            <Typography variant="h5">Settings</Typography>
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <AccountCircleRoundedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <Typography variant="subtitle1">Edit Profile</Typography>
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <BookmarkBorderRoundedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <Typography variant="subtitle1">Saved</Typography>
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <HistoryRoundedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <Typography variant="subtitle1">History</Typography>
-          </ListItem>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <AddLocationAltRoundedIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <Typography variant="subtitle1">Add Location</Typography>
-          </ListItem>
+          <Container sx={{ px: 0.5 }}>
+            <ListItem>
+              <Typography variant="h5">Settings</Typography>
+            </ListItem>
+          </Container>
+          <Divider />
+          <Container sx={{ py: 2 }}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <BookmarkBorderRoundedIcon fontSize="small" />
+                </Avatar>
+              </ListItemAvatar>
+              <Typography variant="subtitle1">Saved</Typography>
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <HistoryRoundedIcon fontSize="small" />
+                </Avatar>
+              </ListItemAvatar>
+              <Typography variant="subtitle1">History</Typography>
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ReviewsRoundedIcon fontSize="small" />
+                </Avatar>
+              </ListItemAvatar>
+              <Typography variant="subtitle1">Your Reviews</Typography>
+            </ListItem>
+          </Container>
+          <Divider />
+          <Container sx={{ py: 2 }}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <LinkRoundedIcon fontSize="small" />
+                </Avatar>
+              </ListItemAvatar>
+              <Typography variant="subtitle1">Share Map</Typography>
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <AddLocationAltRoundedIcon fontSize="small" />
+                </Avatar>
+              </ListItemAvatar>
+              <Typography variant="subtitle1">Add Location</Typography>
+            </ListItem>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <AccountCircleRoundedIcon fontSize="small" />
+                </Avatar>
+              </ListItemAvatar>
+              <Typography variant="subtitle1">Edit Profile</Typography>
+            </ListItem>
+          </Container>
+          <Divider />
+          <Container sx={{ py: 2 }}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <InfoRoundedIcon fontSize="small" />
+                </Avatar>
+              </ListItemAvatar>
+              <Typography variant="subtitle1">Help</Typography>
+            </ListItem>
+          </Container>
         </Drawer>
       </ThemeProvider>
     </>
