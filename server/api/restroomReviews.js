12f933ea626c2dcaac64b57bff445015b7fcb9bb
Ashley H <ashleyho887@gmail.com>
2023-05-15 09:34:25 -0700

    removing middleware auth from everything but users

--- server/api/restroomReviews.js
+++ server/api/restroomReviews.js
@@ -2,36 +2,6 @@ const router = require('express').Router()
 const { models: { Comments, User, Review, Restroom } } = require('../db')
 module.exports = router
 
-// middleware function to check if user isAdmin
-const isAdmin = async (req, res, next) => {
-    try {
-        const user = await User.findByToken(req.headers.authorization);
-        if (!user.isAdmin) {
-            const error = new Error('Not authorized');
-            error.status = 401;
-            throw error;
-        }
-        next();
-    } catch (err) {
-        next(err);
-    }
-};
-
-// middleware function to check if user is the same user or isAdmin
-const isUserOrAdmin = async (req, res, next) => {
-    try {
-        const user = await User.findByToken(req.headers.authorization);
-        if (!user.isAdmin && user.id !== Number(req.params.id)) {
-            const error = new Error('Not authorized');
-            error.status = 401;
-            throw error;
-        }
-        next();
-    } catch (err) {
-        next(err);
-    }
-};
-
 router.get('/:restroomId/reviews', async (req, res, next) => {
   try {
     const reviews = await Review.findAll({
--- server/api/restrooms.js
+++ server/api/restrooms.js
@@ -1,37 +1,7 @@
 const router = require('express').Router();
 const { models: { User, Restroom } } = require('../db')
 
-// middleware function to check if user isAdmin
-const isAdmin = async (req, res, next) => {
-  try {
-    const user = await User.findByToken(req.headers.authorization);
-    if (!user.isAdmin) {
-      const error = new Error('Not authorized');
-      error.status = 401;
-      throw error;
-    }
-    next();
-  } catch (err) {
-    next(err);
-  }
-};
-
-// middleware function to check if user is the same user or isAdmin
-const isUserOrAdmin = async (req, res, next) => {
-  try {
-    const user = await User.findByToken(req.headers.authorization);
-    if (!user.isAdmin && user.id !== Number(req.params.id)) {
-      const error = new Error('Not authorized');
-      error.status = 401;
-      throw error;
-    }
-    next();
-  } catch (err) {
-    next(err);
-  }
-};
-
-router.get('/', isAdmin, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   console.log("Restroom backend API is running")
   try {
     const restrooms = await Restroom.findAll();
@@ -40,6 +10,7 @@ router.get('/', isAdmin, async (req, res, next) => {
     next(error)
   }
 })
+
 router.get('/:id', async (req, res, next) => {
   try {
     console.log("SINGLE Restroom BACKEND WORKING")
@@ -59,6 +30,7 @@ router.post("/", async (req, res, next) => {
     next(err);
   }
 });
+
 router.delete("/:id", async (req, res, next) => {
   try {
     const response = await Restroom.destroy({
