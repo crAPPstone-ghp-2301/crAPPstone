39fa7d0861bff2bd2159169f946dec0fb033fe6d
fionaxu2023 <xuliefan@hotmail.com>
2023-05-16 06:31:55 -0400

    change in map

--- client/app/Map.tsx
+++ client/app/Map.tsx
@@ -59,7 +59,7 @@ const Map = () => {
     });
 
     map.current.addControl(geocoder, "top-right");
-    geocoder.container.setAttribute("id", "geocoder-search");
+    geocoder.container.setAttribute('id', 'geocoder-search')
 
     let popup = new mapboxgl.Popup({ offset: [0, -15] });
 
@@ -99,92 +99,92 @@ const Map = () => {
         .addTo(map.current);
     });
 
-    map.current.on("load", () => {
-      const marker = new mapboxgl.Marker({
-        color: " #CB997E",
-      });
+    // map.current.on("load", () => {
+    //   const marker = new mapboxgl.Marker({
+    //     color: " #CB997E",
+    //   });
 
-      geocoder.on("result", async (event) => {
-        const point = event.result.center;
-        console.log(point);
-        const tileset = "fxu2023.509pfoqy";
-        const radius = 1609;
-        const limit = 50;
-        marker.setLngLat(point).addTo(map.current);
-        const query = await fetch(
-          `https://api.mapbox.com/v4/${tileset}/tilequery/${point[0]},${point[1]}.json?radius=${radius}&limit=${limit}&access_token=${mapboxgl.accessToken}`,
-          { method: "GET" }
-        );
-        console.log(query);
-        const json = await query.json();
-        map.current.getSource("tilequery").setData(json);
-      });
+    //   geocoder.on("result", async (event) => {
+    //     const point = event.result.center;
+    //     console.log(point);
+    //     const tileset = "fxu2023.509pfoqy";
+    //     const radius = 1609;
+    //     const limit = 50;
+    //     marker.setLngLat(point).addTo(map.current);
+    //     const query = await fetch(
+    //       `https://api.mapbox.com/v4/${tileset}/tilequery/${point[0]},${point[1]}.json?radius=${radius}&limit=${limit}&access_token=${mapboxgl.accessToken}`,
+    //       { method: "GET" }
+    //     );
+    //     console.log(query);
+    //     const json = await query.json();
+    //     map.current.getSource("tilequery").setData(json);
+    //   });
 
-      map.current.addSource("tilequery", {
-        type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: [],
-        },
-      });
+    //   map.current.addSource("tilequery", {
+    //     type: "geojson",
+    //     data: {
+    //       type: "FeatureCollection",
+    //       features: [],
+    //     },
+    //   });
 
-      map.current.addLayer({
-        id: "tilequery-points",
-        type: "circle",
-        source: "tilequery",
-        paint: {
-          "circle-stroke-color": "white",
-          "circle-stroke-width": {
-            stops: [
-              [0, 0.1],
-              [18, 3],
-            ],
-            base: 5,
-          },
-          "circle-radius": {
-            stops: [
-              [12, 10],
-              [22, 200],
-            ],
-            base: 5,
-          },
-          "circle-color": [
-            "match",
-            ["get", "Placetype"],
-            "Mall",
-            "#0BB000",
-            "hotel",
-            "#F89446",
-            "restroom",
-            "#EA0000",
-            "#FF0000", // default color if no match
-          ],
-        },
-      });
+    //   map.current.addLayer({
+    //     id: "tilequery-points",
+    //     type: "circle",
+    //     source: "tilequery",
+    //     paint: {
+    //       "circle-stroke-color": "white",
+    //       "circle-stroke-width": {
+    //         stops: [
+    //           [0, 0.1],
+    //           [18, 3],
+    //         ],
+    //         base: 5,
+    //       },
+    //       "circle-radius": {
+    //         stops: [
+    //           [12, 10],
+    //           [22, 200],
+    //         ],
+    //         base: 5,
+    //       },
+    //       "circle-color": [
+    //         "match",
+    //         ["get", "Placetype"],
+    //         "Mall",
+    //         "#0BB000",
+    //         "hotel",
+    //         "#F89446",
+    //         "restroom",
+    //         "#EA0000",
+    //         "#FF0000", // default color if no match
+    //       ],
+    //     },
+    //   });
 
-      const popup = new mapboxgl.Popup();
+    //   const popup = new mapboxgl.Popup();
 
-      map.current.on("mouseenter", "tilequery-points", (event) => {
-        map.current.getCanvas().style.cursor = "pointer";
-        const properties = event.features[0].properties;
-        const obj = JSON.parse(properties.tilequery);
-        const coordinates = new mapboxgl.LngLat(
-          properties.Longitude,
-          properties.Latitude
-        );
-        console.log(properties);
-        const content = `<h3>${properties.STORE_NAME}</h3><h4>${
-          properties.Placetype
-        }</h4><p>${properties.STORE_LOCATION}</p><p>${(
-          obj.distance / 1609.344
-        ).toFixed(2)} mi. from location</p>`;
+    //   map.current.on("mouseenter", "tilequery-points", (event) => {
+    //     map.current.getCanvas().style.cursor = "pointer";
+    //     const properties = event.features[0].properties;
+    //     const obj = JSON.parse(properties.tilequery);
+    //     const coordinates = new mapboxgl.LngLat(
+    //       properties.Longitude,
+    //       properties.Latitude
+    //     );
+    //     console.log(properties);
+    //     const content = `<h3>${properties.STORE_NAME}</h3><h4>${
+    //       properties.Placetype
+    //     }</h4><p>${properties.STORE_LOCATION}</p><p>${(
+    //       obj.distance / 1609.344
+    //     ).toFixed(2)} mi. from location</p>`;
 
-        popup.setLngLat(coordinates).setHTML(content).addTo(map.current);
-      });
-    });
+    //     popup.setLngLat(coordinates).setHTML(content).addTo(map.current);
+    //   });
+    // });
 
     $(document).on("click", "#restroom-mall-nyc", (e) => {
-      const clickedLayer = e.target.id;
+      const clickedLayer = "restroom-mall-nyc";
       e.preventDefault();
       e.stopPropagation();
 
@@ -209,9 +209,11 @@ const Map = () => {
         );
       }
     });
+
+  
 
     $(document).on("click", "#restroom-hotel-nyc", (e) => {
-      const clickedLayer = e.target.id;
+      const clickedLayer = "restroom-hotel-nyc"
       e.preventDefault();
       e.stopPropagation();
 
@@ -238,8 +240,9 @@ const Map = () => {
       }
     });
 
+
     $(document).on("click", "#public-restroom-nyc", (e) => {
-      const clickedLayer = e.target.id;
+      const clickedLayer = "public-restroom-nyc";
       e.preventDefault();
       e.stopPropagation();
 
@@ -266,28 +269,32 @@ const Map = () => {
       }
     });
 
+  
     function direction_reset() {
       directions.actions.clearOrigin();
       directions.actions.clearDestination();
-      directions.container.querySelector("input").value = "";
+      directions.container.querySelector('input').value = '';
     }
-
-    $(document).on("click", "#get-direction", function () {
+    
+    $(document).on('click', '#get-direction', function() {
       // Adding Direction form and instructions on map
-      map.current.addControl(directions, "top-right");
-      directions.container.setAttribute("id", "direction-container");
+      map.current.addControl(directions, 'top-right');
+      directions.container.setAttribute('id', 'direction-container');
       $(geocoder.container).hide();
       $(this).hide();
-      $("#end-direction").removeClass("d-none");
+      $('#end-direction').removeClass('d-none');
     });
-
-    $(document).on("click", "#end-direction", function () {
+    
+    $(document).on('click', '#end-direction', function() {
       direction_reset();
-      $(this).addClass("d-none");
-      $("#get-direction").show();
+      $(this).addClass('d-none');
+      $('#get-direction').show();
       $(geocoder.container).show();
       map.current.removeControl(directions);
     });
+    
+
+
   }, []);
 
   return (
@@ -344,10 +351,10 @@ const Map = () => {
           </Typography>
         </SecondaryButton>
       </Box>
-      <Box>
+      
         <SecondaryButton
           variant="contained"
-          sx={{ px: 1, py: 0.5, mx: 0.5, backgroundColor: "#FFF" }}
+          sx={{ px: 1, py: 0.5,  backgroundColor: "#FFF" }}
           id="get-direction"
         >
           <AssistantDirectionIcon />
@@ -357,7 +364,7 @@ const Map = () => {
         </SecondaryButton>
         <SecondaryButton
           variant="contained"
-          sx={{ px: 1, py: 0.5, mx: 0.5, backgroundColor: "#FFF" }}
+          sx={{ px: 1, py: 0.5,  backgroundColor: "#FFF" }}
           className="d-none"
           id="end-direction"
         >
@@ -366,7 +373,7 @@ const Map = () => {
             For Search
           </Typography>
         </SecondaryButton>
-      </Box>
+      
       <Box ref={mapContainer} className="map-container"></Box>
     </Box>
   );
