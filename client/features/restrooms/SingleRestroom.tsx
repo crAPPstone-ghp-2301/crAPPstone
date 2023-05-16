01309ffb4f731abffbe77c8b465fa249e9316039
ssane005 <ssane005@fiu.edu>
2023-05-16 12:08:44 -0400

    trying to get reviews to show on restroom component

--- client/features/restrooms/SingleRestroom.tsx
+++ client/features/restrooms/SingleRestroom.tsx
@@ -16,38 +16,41 @@ import {
 } from "@mui/material";
 import { PrimaryButton } from "../styles/StyleGuide";
 import { fetchAllReviews } from "../review/reviewSlice";
+import { fetchAllReviewsOfRestroomId } from "../review/reviewSlice";
 
 const SingleRestroom = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
-  console.log(id);
+  const reviews = useSelector((state) => state.review.allReviews);
+  console.log(reviews);
   const restroom = useSelector(selectSingleRestroom);
   console.log("SINGLE RESTROOM===============>", restroom);
 
   useEffect(() => {
     dispatch(getSingleRestroom(id));
+    dispatch(fetchAllReviewsOfRestroomId(id));
     dispatch(fetchAllReviews());
   }, [dispatch, id]);
-  //merge
+
   return (
     <>
       <ThemeProvider theme={crAppTheme}>
         <Card
           sx={{
             maxWidth: 600,
-            maxHeight: 500,
             margin: 10,
             border: "none",
             "&:hover": {
               border: "2px solid",
             },
-            position: 'fixed',
+            position: "fixed",
             zIndex: 1,
             overflow: "auto",
-            scrollbarWidth: "none", 
+            scrollbarWidth: "none",
             "&::-webkit-scrollbar": {
-              display: "none", 
-              },
+              display: "none",
+            },
+            height: "100%",
           }}
         >
           <CardMedia
@@ -55,10 +58,11 @@ const SingleRestroom = () => {
             image={restroom.imageUrl}
             sx={{ height: 200, objectFit: "cover" }}
           />
-          <CardContent sx={{ 
-            height: 250,  
+          <CardContent
+            sx={{
+              height: 250,
             }}
-            >
+          >
             <Typography
               gutterBottom
               variant="body"
@@ -78,12 +82,42 @@ const SingleRestroom = () => {
               {restroom.description}
             </Typography>
             <Link to={`/restrooms/${restroom.id}/reviews`}>
-              <PrimaryButton>
-                Reviews
-              </PrimaryButton>
+              <PrimaryButton>Reviews</PrimaryButton>
             </Link>
           </CardContent>
         </Card>
+        <Box
+          style={{ height: "310px", overflowY: "scroll", paddingRight: "20px" }}
+        >
+          {reviews
+            .filter((review) => review.restroomId === id) // Filter reviews based on restroomId matching id
+            .map((review) => (
+              <Card
+                key={review.id}
+                sx={{
+                  cursor: "pointer",
+                  paddingBottom: "10px",
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  src={review.imageURL}
+                  alt="Picture unavailable!"
+                  onError={(e) => {
+                    e.target.src =
+                      "https://img.freepik.com/free-vector/cute-cat-poop-cartoon-icon-illustration_138676-2655.jpg?w=2000";
+                  }}
+                />
+                <Typography variant="h5">
+                  {review.user ? review.user.username : "Anonymous"}
+                </Typography>
+                <Typography variant="subtitle1">{review.reviewText}</Typography>
+                <Typography variant="subtitle1">
+                  Report: {review.reportStatus}
+                </Typography>
+              </Card>
+            ))}
+        </Box>
       </ThemeProvider>
     </>
   );
