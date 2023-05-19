e8e5c0c77d8ea488f48b32108eea96b284a04b12
fionaxu2023 <xuliefan@hotmail.com>
2023-05-18 22:03:50 -0400

    change in zindex

--- client/features/review/AddReview.tsx
+++ client/features/review/AddReview.tsx
@@ -131,7 +131,7 @@ const AddReview = () => {
         id="add-review-container"
         sx={{
           position: "absolute",
-          zIndex: 1,
+          zIndex: 2,
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
